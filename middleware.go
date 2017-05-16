dd5a2d673a1da31e299a61e3ff9f157604a39b40
TBoileau <t-boileau@email.com>
2017-05-16 16:06:06 +0200

    middlewares

--- middleware.go
+++ middleware.go
@@ -1,11 +1,24 @@
 package framework
 
-import "reflect"
+import (
+	"io/ioutil"
+	"log"
+	"reflect"
+)
 
 type Middleware struct {
+	ConfigFile string
 }
 
 type MiddlewareInterface interface {
 	GetName() string
 	Register() reflect.Value
+}
+
+func (middleware *Middleware) LoadParameters() (yamlFile []byte) {
+	yamlFile, err := ioutil.ReadFile(middleware.ConfigFile)
+	if err != nil {
+		log.Panic("No config file : " + middleware.ConfigFile)
+	}
+	return
 }
